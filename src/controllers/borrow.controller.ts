4596e7c632155e383238aaaeb3685663f1c78569
devsafix <devsafix@gmail.com>
2025-06-21 14:07:54 +0600

    get borrow summery api created

--- src/controllers/borrow.controller.ts
+++ src/controllers/borrow.controller.ts
@@ -36,6 +36,50 @@ const borrowBook = async (req: Request, res: Response) => {
   }
 };
 
+const getBorrowSummary = async (req: Request, res: Response) => {
+  try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookInfo",
+        },
+      },
+      { $unwind: "$bookInfo" },
+      {
+        $project: {
+          _id: 0,
+          book: {
+            title: "$bookInfo.title",
+            isbn: "$bookInfo.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: summary,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Error retrieving borrow summary",
+      error: err,
+    });
+  }
+};
+
 export const borrowController = {
   borrowBook,
+  getBorrowSummary,
 };
